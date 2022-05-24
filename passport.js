1aea8cbc000d74ae8718ed45723024647c6a208c
lassev05 <vestergaardlasse2@gmail.com>
2022-05-24 17:33:40 +0200

    push code

--- passport.js
+++ passport.js
@@ -1,113 +1,151 @@
 // Passport.js logic
-const mongo = require('./db')
-const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy
-const GoogleStrategy = require('passport-google-oauth20').Strategy
-const ObjectID = require('mongodb').ObjectID
-const bcrypt = require('bcrypt-nodejs')
-const secrets = require('./secrets')
+const mongo = require("./db");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const ObjectID = require("mongodb").ObjectID;
+const bcrypt = require("bcrypt-nodejs");
 
 // Local login/signup (username and password)
-passport.use(new LocalStrategy({
-  passReqToCallback: true
-}, authenticate))
+passport.use(
+  new LocalStrategy(
+    {
+      passReqToCallback: true,
+    },
+    authenticate
+  )
+);
 
-passport.use("local-register", new LocalStrategy({
-  passReqToCallback: true
-}, register))
+passport.use(
+  "local-register",
+  new LocalStrategy(
+    {
+      passReqToCallback: true,
+    },
+    register
+  )
+);
 
 // Google OAuth 2.0 strategey
-passport.use(new GoogleStrategy({
-    clientID: secrets.googleClientID,
-    clientSecret: secrets.googleClientSecret,
-    callbackURL: "http://forumerly.jayvolr.me/auth/google/callback"
-  },
-  function (accessToken, refreshToken, profile, done) {
-    var date = new Date()
-    // Search for user with given google user id
-    mongo.db.collection('users')
-      .findOne({ oauth_provider: 'google', oauth_id: profile.id }, (err, result) => {
-        // If they exist (have signed in before), return that user
-        if (result) {
-          done(null, result)
-        // Else, create the user
-        }else {
-          var newUser = {
-            oauth_provider: 'google',
-            oauth_id: profile.id,
-            username: profile.displayName,
-            lcUsername: profile.displayName.toLowerCase(),
-            joinDate: date,
-            img: profile.photos[0].value + '0'
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID:
+        "948522409811-l4kshn91lsuqtk220rfo8i4mrpi3951v.apps.googleusercontent.com",
+      clientSecret: "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",
+      callbackURL: "http://localhost:3000/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      var date = new Date();
+      // Search for user with given google user id
+      mongo.db
+        .collection("users")
+        .findOne(
+          { oauth_provider: "google", oauth_id: profile.id },
+          (err, result) => {
+            // If they exist (have signed in before), return that user
+            if (result) {
+              done(null, result);
+              // Else, create the user
+            } else {
+              var newUser = {
+                oauth_provider: "google",
+                oauth_id: profile.id,
+                username: profile.displayName,
+                lcUsername: profile.displayName.toLowerCase(),
+                joinDate: date,
+                img: profile.photos[0].value + "0",
+              };
+              mongo.db.collection("users").insert(newUser, (err, result) => {
+                return done(null, result.ops[0]);
+              });
+            }
           }
-          mongo.db.collection('users')
-            .insert(newUser, (err, result) => {
-              return done(null, result.ops[0])
-            })
-        }
-      })
-  }
-))
+        );
+    }
+  )
+);
 
 // Called by auth.js via passport when a user attempts to login
 function authenticate(req, username, password, done) {
-  mongo.db.collection("users")
-    .findOne({ lcUsername: username.toLowerCase() }, {collation: {locale: "en", strength: 2}}, (err, user) => {
-      if (err) {return done(err)}
-      if (!user || !bcrypt.compareSync(password, user.password)) {
-        return done(null, false, { message: 'Invalid username or password.' })
+  mongo.db
+    .collection("users")
+    .findOne(
+      { lcUsername: username.toLowerCase() },
+      { collation: { locale: "en", strength: 2 } },
+      (err, user) => {
+        if (err) {
+          return done(err);
+        }
+        if (!user || !bcrypt.compareSync(password, user.password)) {
+          return done(null, false, {
+            message: "Invalid username or password.",
+          });
+        }
+        return done(null, user);
       }
-      return done(null, user)
-    })
+    );
 }
 
 // Called by auth.js via passport when a user attempts to create a new account
 function register(req, username, password, done) {
   // Password and username validation
   if (password !== req.body.password2) {
-    return done(null, false, { message: 'Passwords do not match.' })
-  }else if (username.length > 20) {
-    return done(null, false, { message: 'Username cannot be longer than twenty characters.' })
-  }else if (username.length < 3) {
-    return done(null, false, { message: 'Username must be atleast three characters.' })
+    return done(null, false, { message: "Passwords do not match." });
+  } else if (username.length > 20) {
+    return done(null, false, {
+      message: "Username cannot be longer than twenty characters.",
+    });
+  } else if (username.length < 3) {
+    return done(null, false, {
+      message: "Username must be atleast three characters.",
+    });
   }
 
-  var date = new Date()
+  var date = new Date();
   // Checks if username is already in use
-  mongo.db.collection('users')
-    .findOne({ lcUsername: username.toLowerCase() }, {collation: {locale: "en", strength: 2}}, (err, user) => {
-      if (err) {return done(err)}
-      if (user) {
-        console.log('username catch')
-        return done(null, false, {message:'Username is already in use.'})
-      }
-      // Past here will only run if the username was found to be not in use
-      var newUser = {
-        username: req.body.username,
-        lcUsername: req.body.username.toLowerCase(),
-        password: bcrypt.hashSync(password),
-        joinDate: date,
-        img: '/images/profile.png'
-      }
+  mongo.db
+    .collection("users")
+    .findOne(
+      { lcUsername: username.toLowerCase() },
+      { collation: { locale: "en", strength: 2 } },
+      (err, user) => {
+        if (err) {
+          return done(err);
+        }
+        if (user) {
+          console.log("username catch");
+          return done(null, false, { message: "Username is already in use." });
+        }
+        // Past here will only run if the username was found to be not in use
+        var newUser = {
+          username: req.body.username,
+          lcUsername: req.body.username.toLowerCase(),
+          password: bcrypt.hashSync(password),
+          joinDate: date,
+          img: "/images/profile.png",
+        };
 
-      // Insert the new username into the database
-      mongo.db.collection('users')
-        .insert(newUser, (err, result) => {
-          if (err) {return done(err)}
-          return done(null, result.ops[0])
-        })
-
-    })
+        // Insert the new username into the database
+        mongo.db.collection("users").insert(newUser, (err, result) => {
+          if (err) {
+            return done(err);
+          }
+          return done(null, result.ops[0]);
+        });
+      }
+    );
 }
 
 // Passport serialize and deserialize functions
-passport.serializeUser(function(user, done) {
-  done(null, user._id.toHexString())
-})
+passport.serializeUser(function (user, done) {
+  done(null, user._id.toHexString());
+});
 
-passport.deserializeUser(function(id, done) {
-  mongo.db.collection('users')
+passport.deserializeUser(function (id, done) {
+  mongo.db
+    .collection("users")
     .findOne({ _id: new ObjectID.createFromHexString(id) }, (err, user) => {
-      done(err, user)
-    })
-})
+      done(err, user);
+    });
+});
--- routes/auth.js
+++ routes/auth.js
@@ -1,48 +1,61 @@
 // Routes for signup and login/logout
-const express = require('express')
-const router = express.Router()
-const mongo = require('../db')
-const passport = require('passport')
-require('../passport')
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+require("../passport");
 
 router
   // POST signup via passport local strategey
-  .post('/signup', passport.authenticate('local-register', {
-    successRedirect: '/',
-    failureRedirect: '/register',
-    failureFlash: true,
-    successFlash: 'Account created!'
-  }))
+  .post(
+    "/signup",
+    passport.authenticate("local-register", {
+      successRedirect: "/",
+      failureRedirect: "/register",
+      failureFlash: true,
+      successFlash: "Account created!",
+    })
+  )
 
   // POST login via passport local strategey
-  .post('/login', passport.authenticate('local', {
-    successRedirect: 'back',
-    failureRedirect: 'back',
-    failureFlash: true
-  }))
+  .post(
+    "/login",
+    passport.authenticate("local", {
+      successRedirect: "back",
+      failureRedirect: "back",
+      failureFlash: true,
+    })
+  )
 
   // Google OAuth routes
-  .get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] })) 
+  .get(
+    "/auth/google",
+    passport.authenticate("google", { scope: ["profile", "email"] })
+  )
 
-  .get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/register' }), (req, res) => {
-      res.redirect('/')
-  })
+  .get(
+    "/auth/google/callback",
+    passport.authenticate("google", { failureRedirect: "/register" }),
+    (req, res) => {
+      res.redirect("/");
+    }
+  )
 
   // GET Logout and redirect
-  .get('/logout', (req, res) => {
-    req.logout()
+  .get("/logout", (req, res) => {
+    req.logout();
     // If the previous page is one of these, redirect to home
-    if(req.header("Referer") === 'http://localhost:3000/other/admins'
-    || req.header("Referer") === 'http://forumerly.jayvolr.com/other/admins'
-    || req.header("Referer") === 'http://localhost:3000/settings'
-    || req.header("Referer") === 'http://forumerly.jayvolr.com/settings'
+    if (
+      req.header("Referer") === "http://localhost:3000/other/admins" ||
+      req.header("Referer") === "http://forumerly.jayvolr.com/other/admins" ||
+      req.header("Referer") === "http://localhost:3000/settings" ||
+      req.header("Referer") === "http://forumerly.jayvolr.com/settings"
     ) {
-      res.redirect('/')
-    // Else return to the previous page
-    }else {
-      res.redirect('back')
+      res.redirect("/");
+      // Else return to the previous page
+    } else {
+      res.redirect("back");
     }
-  })
+  });
 
 // Export these routes to be used in app.js
-module.exports = router
+module.exports = router;
